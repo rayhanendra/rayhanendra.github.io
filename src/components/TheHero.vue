ced668f9bc11461741b0716f0f99ebf671d1da02
Rayhanendra <mrayhann@gmail.com>
2023-03-16 22:34:20 +0700

    feat: add tailwind & gsap, add hero section, clean up code

---
+++ src/components/TheHero.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { gsap } from 'gsap'
+
+const textLoopRef = ref(null) as any
+
+const tools = ref([
+  'HTML',
+  'CSS',
+  'LESS',
+  'Javascript',
+  'Typescript',
+  'ReactJs',
+  'VueJs',
+  'NextJs',
+  'NuxtJs',
+  'MaterialUI',
+  'Vuetify',
+  'TailwindCSS',
+  'Storybook',
+  'GSAP'
+])
+
+onMounted(() => {
+  const textLoop = gsap.timeline({ repeat: -1 })
+  const textElements = Array.from(textLoopRef.value.children)
+  console.log('ini text elements', textElements)
+
+  const duration = 12
+  textElements.forEach((el, index) => {
+    // Note: this is just to set the initial position of the elements
+    gsap.set(el as any, { xPercent: index * 340 })
+    // Note: this is the animation that will loop
+    textLoop.to(
+      el as any,
+      {
+        xPercent: (index - textElements.length) * 340,
+        duration: duration,
+        ease: 'none'
+      },
+      0
+    )
+  })
+})
+</script>
+
+<template>
+  <div class="tw-container tw-mx-auto tw-py-28">
+    <div class="tw-grid tw-grid-cols-12 tw-gap-3 tw-px-12">
+      <div class="tw-col-span-12 sm:tw-col-span-6">
+        <div class="tw-text-2xl">
+          Hello, I’m <span class="tw-font-semibold">Nendra</span> | ネンドラ
+          <span class="tw-text-xs">(Nendora)</span>
+        </div>
+        <div class="tw-text-xl">
+          a <span class="tw-font-semibold">Frontend Engineer </span>who is
+        </div>
+        <div class="tw-text-4xl tw-max-w-[645px] tw-font-bold">
+          Striving for excellence through implementing best practices in all aspects.
+        </div>
+
+        <button
+          class="tw-mt-10 tw-p-4 tw-py-[12px] tw-px-8 tw-border tw-border-[#64FFDA] tw-text-[#64FFDA] tw-text-base tw-font-semibold tw-transition tw-duration-300 tw-ease-in-out tw-transform hover:tw-text-[#000000] hover:tw-bg-[#64FFDA] hover:tw-scale-105"
+        >
+          Check Resume!
+        </button>
+      </div>
+      <div class="tw-col-span-12 sm:tw-col-span-6 tw-flex tw-justify-end">
+        <!-- <div class="tw-relative tw-w-96 tw-h-72">
+          <img
+            src="@/assets/images/hero.png"
+            alt="hero"
+            class="tw-absolute -tw-bottom-4 -tw-right-4"
+          />
+          <div class="tw-border tw-border-[#64FFDA] tw-w-full tw-h-full"></div>
+        </div> -->
+        <div class="tw-relative tw-w-96 tw-h-72">
+          <img src="@/assets/images/hero.png" alt="hero" />
+          <div
+            class="tw-absolute -tw-top-4 -tw-left-4 tw-border tw-border-[#64FFDA] tw-w-full tw-h-full"
+          ></div>
+        </div>
+      </div>
+    </div>
+    <div class="tw-overflow-clip tw-py-4 tw-mt-14 tw-border-t tw-border-b tw-border-[#233554]">
+      <div ref="textLoopRef" class="tw-whitespace-nowrap">
+        <span
+          v-for="tool in tools"
+          :key="tool"
+          class="tw-inline-block tw-w-6 tw-mr-14 tw-text-[#64FFDA]"
+        >
+          <div class="">
+            {{ tool }}
+          </div>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TheHero />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheHero from '@/components/TheHero.vue'
+</script>
+
+<style scoped></style>
