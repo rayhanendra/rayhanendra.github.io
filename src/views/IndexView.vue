4ddd096b68c07362e2246b20d3d4a6caa7cd3c9f
Rayhanendra <mrayhann@gmail.com>
2023-10-23 02:23:25 +0700

    feat: add new 3D section with three js

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <SectionRocket />
+    <SectionRocketThree />
+    <!-- <SectionRocket /> -->
     <SectionHero />
     <SectionAbout />
     <SectionWork />
@@ -26,12 +27,13 @@
 </template>
 
 <script setup lang="ts">
-import SectionAbout from '@/components/SectionAbout.vue'
-import SectionArchive from '@/components/SectionArchive.vue'
+import SectionRocketThree from '@/components/SectionRocketThree.vue'
+// import SectionRocket from '@/components/SectionRocket.vue'
 import SectionHero from '@/components/SectionHero.vue'
-import SectionProject from '@/components/SectionProject.vue'
-import SectionRocket from '@/components/SectionRocket.vue'
+import SectionAbout from '@/components/SectionAbout.vue'
 import SectionWork from '@/components/SectionWork.vue'
+import SectionProject from '@/components/SectionProject.vue'
+import SectionArchive from '@/components/SectionArchive.vue'
 
 // Note: Example of using GSAP ScrollTrigger with array of sections
 // import { onMounted } from 'vue'
